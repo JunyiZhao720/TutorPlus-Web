0d477a416b7f7c17fb09a0c00e335fcca8686795
zyzhang343 <38702666+zyzhang343@users.noreply.github.com>
2018-12-05 13:47:09 -0800

    fix

--- TutorPlusWeb/web2py/applications/TutorPlus/controllers/default.py
+++ TutorPlusWeb/web2py/applications/TutorPlus/controllers/default.py
@@ -16,10 +16,10 @@ def index():
     # else:
     #     # if not contains, then redirects to login page
     #     redirect(URL('default', 'login'))
-    if request.vars.state is not None:
-        login_state = request.vars.state.lower().startswith('t')
-        if not login_state:
-            redirect(URL('default', 'login'))
+    #if request.vars.state is not None:
+    #    login_state = request.vars.state.lower().startswith('t')
+    #    if not login_state:
+    #        redirect(URL('default', 'login'))
     return dict()
 
 def login():
@@ -75,3 +75,97 @@ def download():
     http://..../[app]/default/download/[filename]
     """
     return response.download(request, db)
+
+'''
+def signup():
+    # Use a service account
+    firebaseKeyPath = '../private/tutorPlusKey.json'
+    cred = credentials.Certificate(firebaseKeyPath)
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+
+    user_email = request.vars['user_email'].decode()
+    user_pwd = request.vars['user_pwd'].decode()
+    user_first_name = request.vars['user_first_name'].decode()
+    user_last_name = request.vars['user_last_name'].decode()
+
+    try:
+        user = auth.get_user_by_email(user_email)
+    except:
+        #email is not used
+        if len(user_pwd) < 6:
+            ##there should be front end checking
+            ##user password is not long enough, firebase request 6 at least
+            pass
+        else:
+            #create account
+            user = auth.create_user(
+                email=user_email,
+                email_verified=False,
+                password=user_pwd,
+                disabled=False)
+            user_uid = user.uid.decode()
+
+            ## assign a cookie to browser here!!!!!
+
+            #put data to firestore
+            user_data = {
+                u'first_name': user_first_name,
+                u'last_name': user_last_name,
+                u'email': user_email
+            }
+            db.collection(u'users').document(user_uid).set(user_data)
+            #user is created
+    else:
+        #email already used!!
+        pass
+'''
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
+import json
+import os
+
+userUidArr=[u'47M3QdWhfkMbrSDvuTglpwz30Gn1',
+            u'8TyZ3sGCYYPA2p11wIGUPnxPW5A3',
+            u'Ipus0TpfCzXVABANUoE32Qb3V7A2',
+            u'N1OaUlBKfXaXhX24VbleWNwKmnc2',
+            u'NElrjI2dYqNxTBTO2mVsteDgWDd2',
+            u'dJAdQJFv6Lgk8fYRF8n3eTzLIDm1'
+    ]
+
+
+
+'''
+def profile():
+
+    db = firestore.client()
+
+    newArr = []
+
+    doc_ref = db.collection(u'users').document(u'dJAdQJFv6Lgk8fYRF8n3eTzLIDm1')
+    doc = doc_ref.get()
+
+    if doc :
+        return doc.to_dict()
+    else:
+        return "cannot find"
+
+    for uid in userUidArr:
+        doc_ref = db.collection(u'users').document(uid)
+        try:
+            doc = doc_ref.get()
+            newArr.append({uid:doc.to_dict()['email']})
+        except:
+            newArr.append({uid:"cannot find"})
+            pass
+            #print(u'No such document!')
+        #print "Get document: " + uid
+
+    #print "Hello World"
+    #print
+
+    return json.dumps(newArr,indent=4)
+'''
