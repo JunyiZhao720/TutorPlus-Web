38825b005138a0e3f91615b36bacdb5d6bf9bd26
zjylh <zjylh720@gmail.com>
2018-12-10 02:12:07 -0800

    10010 Fixed login

--- TutorPlusWeb/web2py/applications/TutorPlus/static/js/default_index.js
+++ TutorPlusWeb/web2py/applications/TutorPlus/static/js/default_index.js
@@ -246,6 +246,15 @@ var app = function() {
             // form_content: "",
             // post_list: [],
             // star_indices: [1, 2, 3, 4, 5],
+            show1: false,
+            show2: true,
+            show3: false,
+            show4: false,
+            password: 'Password',
+            rules: {
+              min: v => v.length >= 8 || 'Min 8 characters',
+              emailMatch: () => ('The email and password you entered don\'t match')
+            }
         },
         methods: {
             // login-part
